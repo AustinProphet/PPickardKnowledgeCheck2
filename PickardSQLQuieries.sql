1e25c3279260f3198474b4af266d36008cdcb08c
AustinProphet <austinprophet@gmail.com>
2022-10-25 18:49:23 -0400

    uploaded sql file

---
+++ PickardSQLQuieries.sql
@@ -0,0 +1,4 @@
+SELECT CustomerID FROM [Orders] Where OrderID=10310
+SELECT CustomerName FROM [Customers] Where CustomerID = 77
+SELECT SupplierID FROM [Products] Where ProductID = 40
+SELECT Address FROM [Suppliers] Where SupplierID = 19
